fbb26051a0dd7c1bf4111abe99d9b0b408f5ca04
Byle <8009856+TheKyleChau@users.noreply.github.com>
2023-02-01 19:05:47 -0800

    Create new files, and test notification

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,25 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    protected $title = 'Home';
+    protected $data = array();
+
+    public function __construct() {
+      $this->data = array(
+        'title' => $this->title
+      );
+    }
     public function index()
     {
-        return view('welcome_message');
+      helper(form);
+      $linkTags = array(
+        'css' => array(
+        'href' => 'public/css/home.css'),
+        'rel' => 'stylesheet',
+        'type' => 'text/css'
+        )
+      );
+      $this->data['linkTags'] = $linkTags;
+      return view('home/home', $this->data); //Returns home page from Views folder
     }
 }
---
+++ app/Views/home/home.php
@@ -0,0 +1,4 @@
+<?=>$this->extend('themes/basicTheme')?>
+<?=>$this->section('content')?>
+<?=>$this->include('home/homeContent')?>
+<?=>$this->endSection()?>
